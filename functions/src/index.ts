3758fba0924e9f08f617e484f69c6a5143dc0d15
Sam <sgphillips8@gmail.com>
2022-01-22 14:20:27 -0500

    continued work on backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,44 @@ import * as functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/apiRoute';
+import { getClient } from './db';
+import Recipe from './models/recipe';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
+app.get("/recipes", async (req, res) => {
+    // try means: try to do this, and if it doesn't work return the catch
+    try {
+        const client = await getClient();
+
+        // call the mongodb
+          const results = await client
+          .db()
+        // convert the data into an array
+          .collection<Recipe>('recipes')
+          .find()
+          .toArray()
+        // return that data to the browser
+        res.json(results);
+      
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+
+});
+
+app.get("/favorites", async (req, res) => {
+    try {
+        const client = await getClient();
+
+        const results = await client
+        .db()
+        .collection // This is where we left off 1/22
+    }
+})
 
 export const api = functions.https.onRequest(app);
